961879b1153f892cf32d93f42c72f1828d3dfa88
Sathe-Khanom <sathekhanom550@gmail.com>
2025-06-22 18:11:27 +0600

    add

--- code/myapp/src/app/home/home.ts
+++ code/myapp/src/app/home/home.ts
@@ -1,11 +1,1 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-home',
-  standalone: false,
-  templateUrl: './home.html',
-  styleUrl: './home.css'
-})
-export class Home {
-
-}
--- code/template/src/app/app-routing-module.ts
+++ code/template/src/app/app-routing-module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { Home } from './home/home';
 
-const routes: Routes = [
-  {path:'', component : Home}
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
