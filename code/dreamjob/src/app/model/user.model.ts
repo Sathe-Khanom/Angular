2b3c52ecb50770661626fbe80994f64ebdc643a2
Sathe-Khanom <sathekhanom550@gmail.com>
2025-07-17 14:20:22 +0600

    add

---
+++ code/dreamjob/src/app/model/user.model.ts
@@ -0,0 +1,15 @@
+
+export class User{
+
+id !: string;
+name !: string;
+mobile !: string;
+email !: string;
+address !: string;
+education !: string;
+experience !: string;
+role !: string;
+password !: string
+
+
+}
